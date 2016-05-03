ca8061699f84137b34833a9a2168f5ba8b20a5fe
David S <idavid.str@gmail.com>
2016-05-03 15:57:08 -0600

    fixed tabs.

--- src/app.js
+++ src/app.js
@@ -13,18 +13,18 @@ import $ from "jquery";
 // Navigation Clicks
 
 $('#main-navbar').on({
-	
+
 	click(evt) {
 		
 		let href = $(this).attr('href');
-		
+
 		// console.log(href);
 		evt.preventDefault();
-		
-		
-		
+
+
+
 		fetchTemplates( href );
-		
+
 		router.navigate('section', {section: href }, { reload: true });
 			
 	}
@@ -34,43 +34,43 @@ $('#main-navbar').on({
 // Fetch Templates
 
 let fetchTemplates = (templateURL) => {
-	let template = `src/templates/${templateURL}.html`;
-	
-	fetch(template)
-		.then( ( response ) => {
-			return response.text();
-		})
-		.then( (content) => {
-			$('#main-container').html( content );
-		});
+let template = `src/templates/${templateURL}.html`;
+
+fetch(template)
+	.then( ( response ) => {
+		return response.text();
+	})
+	.then( (content) => {
+		$('#main-container').html( content );
+	});
 };
 
 
 // Routes
 const router = new Router5([
-	new RouteNode('home', '/'),
-	new RouteNode('section', '/:section')
+new RouteNode('home', '/'),
+new RouteNode('section', '/:section')
 ],{
-				useHash: true,
-				// hashPrefix: '!',
-				defaultRoute: 'home',
-				defaultParams: { section : ''},
-				base: '',
-				trailingSlash: false,
-				autoCleanUp: true,
-				strictQueryParams: true
-		})
-		.usePlugin(loggerPlugin())
-		.usePlugin(listenersPlugin())
-		.usePlugin(historyPlugin({ forceDeactivate: false }));
+	useHash: true,
+	// hashPrefix: '!',
+	defaultRoute: 'home',
+	defaultParams: { section : ''},
+	base: '',
+	trailingSlash: false,
+	autoCleanUp: true,
+	strictQueryParams: true
+})
+.usePlugin(loggerPlugin())
+.usePlugin(listenersPlugin())
+.usePlugin(historyPlugin({ forceDeactivate: false }));
 
 
 router.start( (err, state) => {
 	if (err) console.error('error ', err);
-	
+
 	let loadPath = (state.path === "/") ? "index" : state.path;
 		
 	console.log('state ', state);
-	
+
 	fetchTemplates( loadPath );
 });
